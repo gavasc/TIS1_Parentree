3ce8f33a3ba44fa5711ea48d6022bb800233658e
Gabriel Vasconcelos <Gabriel Vasconcelos>
2019-10-23 17:14:36 -0300

    Alterações no banco de dados

--- db.js
+++ db.js
@@ -1,56 +1,57 @@
-var dados = {
-    'plantas': [
+var db = {
+    "plantas": 
+    [
         {
-            nomePlanta: 'Jiboia',
-            linkImagem: '',
-            luz: 2,
-            agua: true,
-            nivelDificuldade: 1,
-            flores: 3,
-            tags: ['Meia Luz', 'Pouca Agua', 'Muito Facil'],
-            resumo: 'É uma das plantas mais fáceis de cuidar, suas folhas com detalhes amarelos deixam qualquer ambiente bonito.',
-            descricao: 'A Jiboia é uma das melhores plantas para ter em casa, é extremamente fácil de cuidar e se desenvolve super rápido. Precisa de um solo rico em matéria orgânica. Ela é uma trepadeira, portanto, seu caule crescerá pendurado para baixo, como uma samambaia; também é uma planta resistente, consegue viver dentro ou fora de casa, até mesmo no ar condicionado, porém o mais indicado é mantê-la a meia luz, ou seja, ficar em um lugar iluminado mas sem esposição direta ao sol. É necessário regar a planta 2 a 3 vezes por semana no verão e 1 a 2 vezes no inverno. É muito fácil obter mudinhas dessa planta, corte um galho e o finque na terra ou em um pote com água e logo criará raízes. Não se esqueça que a Jiboia é uma planta tóxica, então a mantenha longe de crianças e animais.',
-            dicas: ['Manter a planta totalmente na sombra deixará suas folhas mais escuras. Não se preocupe, é normal!',
-                'Ela gosta de sentir um pouco de sede, espere a terra ficar seca para regar novamente.',
-                'A Jiboia remove resíduos tóxicos do ar.',
-                'Plante-a em um vaso grande caso queira que ela fique muito volumosa e vice-versa.',
-                'Caso não queira os galhos pendurados pode a planta a cada 5 ou 6 meses, ou coloque uma ripa no meio do vaso e ela crescerá para cima.'
+            "nomePlanta": "Jiboia",
+            "linkImagem": "",
+            "luz": 2,
+            "agua": true,
+            "nivelDificuldade": 1,
+            "flores": 3,
+            "tags": ["Meia luz", "Pouca agua", "Muito Facil"],
+            "resumo": "É uma das plantas mais fáceis de cuidar, suas folhas com detalhes amarelos deixam qualquer ambiente bonito.",
+            "descricao": "A Jiboia é uma das melhores plantas para ter em casa, é extremamente fácil de cuidar e se desenvolve super rápido. Precisa de um solo rico em matéria orgânica. Ela é uma trepadeira, portanto, seu caule crescerá pendurado para baixo, como uma samambaia; também é uma planta resistente, consegue viver dentro ou fora de casa, até mesmo no ar condicionado, porém o mais indicado é mantê-la a meia luz, ou seja, ficar em um lugar iluminado mas sem esposição direta ao sol. É necessário regar a planta 2 a 3 vezes por semana no verão e 1 a 2 vezes no inverno. É muito fácil obter mudinhas dessa planta, corte um galho e o finque na terra ou em um pote com água e logo criará raízes. Não se esqueça que a Jiboia é uma planta tóxica, então a mantenha longe de crianças e animais.",
+            "dicas": ["Manter a planta totalmente na sombra deixará suas folhas mais escuras. Não se preocupe, é normal!",
+                "Ela gosta de sentir um pouco de sede, espere a terra ficar seca para regar novamente.",
+                "A Jiboia remove resíduos tóxicos do ar.",
+                "Plante-a em um vaso grande caso queira que ela fique muito volumosa e vice-versa.",
+                "Caso não queira os galhos pendurados pode a planta a cada 5 ou 6 meses, ou coloque uma ripa no meio do vaso e ela crescerá para cima."
             ]
         },
         {
-            nomePlanta: 'Peperômia melancia',
-            linkImagem: '',
-            luz: 2,
-            agua: true,
-            nivelDificuldade: 1,
-            flores: 2,
-            tags: ['Meia Luz', 'Pouca Agua', 'Muito Facil'],
-            resumo: 'Possui uma folhagem muito linda, é pequena apesar de ser uma trepadeira.',
-            descricao: 'A Peperômia melancia também é conhecida como peperômia-zebra. Suas lindas folhas em formato de coração tem listras, exatemente como uma melancia, sua folhagem é densa e seu porte é pequeno; cresce até 25 cm de altura. É ótima para decorar mesinhas de centro ou a escrivaninha do seu quarto. Nunca a deixe diretamente no sol, suas folhas queimarão. Se mostra uma grande amiga para os iniciantes pois não precisa de muitos cuidados, somente um solo com adubo orgânico, que deve ser mantido úmido, e água 1 vez por semana. A peperômia pode ser plantada o ano inteiro, porém só floresce no verão, embora as flores não sejam muito bonitas. Além de trepadeira, ela é uma planta suculenta, então suas folhas reservam um pouco de água.',
-            dicas: ['Por ser uma planta nativa e tropical, preste mais atenção nela durante o inverno.',
-                'Cubra o solo com palha protetora para ajudar a mantê-lo úmido.',
-                'Mesmo que ela goste de água, plante-a em uma vaso com furinhos no fundo, se acumular água suas raízes podem apodrecer.',
-                'Adicione uma camada de areia no fundo para garantir a drenagem',
-                'Coloque-a perto de uma janela, para receber luz e continuar brilhante e saudável.',
-                'Para retirar mudinhas, corte o caule de uma folha saudável e ainda em crescimento e plante em um solo adubado, em pouca semanas já estrá nascendo uma plantinha nova.'
+            "nomePlanta": "Peperômia melancia",
+            "linkImagem": "",
+            "luz": 2,
+            "agua": true,
+            "nivelDificuldade": 1,
+            "flores": 2,
+            "tags": ["Meia luz", "Pouca agua", "Muito Facil"],
+            "resumo": "Possui uma folhagem muito linda, é pequena apesar de ser uma trepadeira.",
+            "descricao": "A Peperômia melancia também é conhecida como peperômia-zebra. Suas lindas folhas em formato de coração tem listras, exatemente como uma melancia, sua folhagem é densa e seu porte é pequeno; cresce até 25 cm de altura. É ótima para decorar mesinhas de centro ou a escrivaninha do seu quarto. Nunca a deixe diretamente no sol, suas folhas queimarão. Se mostra uma grande amiga para os iniciantes pois não precisa de muitos cuidados, somente um solo com adubo orgânico, que deve ser mantido úmido, e água 1 vez por semana. A peperômia pode ser plantada o ano inteiro, porém só floresce no verão, embora as flores não sejam muito bonitas. Além de trepadeira, ela é uma planta suculenta, então suas folhas reservam um pouco de água.",
+            "dicas": ["Por ser uma planta nativa e tropical, preste mais atenção nela durante o inverno.",
+                "Cubra o solo com palha protetora para ajudar a mantê-lo úmido.",
+                "Mesmo que ela goste de água, plante-a em uma vaso com furinhos no fundo, se acumular água suas raízes podem apodrecer.",
+                "Adicione uma camada de areia no fundo para garantir a drenagem",
+                "Coloque-a perto de uma janela, para receber luz e continuar brilhante e saudável.",
+                "Para retirar mudinhas, corte o caule de uma folha saudável e ainda em crescimento e plante em um solo adubado, em pouca semanas já estrá nascendo uma plantinha nova."
             ]
         },
         {
-            nomePlanta: 'Zamioculca',
-            linkImagem: '',
-            luz: 3,
-            agua: true,
-            nivelDificuldade: 1,
-            flores: 3,
-            tags: ['Sombra', 'Pouca Agua', 'Muito Facil'],
-            resumo: 'Também é conhecida como árvore de dinheiro pelo tom verde das folhas, pode sobreviver muito tempo sem água',
-            descricao: 'A Zamioculca se adapta em praticamente qualquer lugar que não bata sol, ela se dá muito bem em locais pouco ventilados e com baixa iluminação, locais com ar condicionado também. É uma planta tropical e não gosta de frio, então a proteja quando a temperatura cair. Essa planta requer poucos cuidados, consegue resistir muito tempo sem água, pois seu rizoma — um caule subterrâneo — consegue armazenar água. Regar 1 vez por semana é suficiente; mantenha o solo sempre úmido, mas nunca encharque — as folhas começam a ficar amarelas e a raíz morre. Adube o solo com matéria orgânica para mentê-la sempre saudável. A Zamioculca floresce no verão ou no inicio do outono, porém é muito raro ver as flores já que ficam escondidas. Mantenha a planta longe de animais ou crianças, pois é venenosa quando ingerida e a seiva pode causar irritação na pele.',
-            dicas: ['Adube o solo a cada 3 ou 4 meses',
-                'Remova as folhas amareladas ou mortas para não afetar a saúde da planta.',
-                'Gire o vaso de vez em quando para que toda a planta receba sol.',
-                'Não limpe a planta com produtos especiais para isso, ela não se adapta. Passe café puro com um pano macio, vai deixá-las brilhantes.',
-                'Coloque um pouco de areia no fundo para garantir a drenagem.',
-                'Regue 1 a 2 vezes por semana quando estiver muito quente'
+            "nomePlanta": "Zamioculca",
+            "linkImagem": "",
+            "luz": 3,
+            "agua": true,
+            "nivelDificuldade": 1,
+            "flores": 3,
+            "tags": ["Sombra", "Pouca agua", "Muito Facil"],
+            "resumo": "Também é conhecida como árvore de dinheiro pelo tom verde das folhas, pode sobreviver muito tempo sem água",
+            "descricao": "A Zamioculca se adapta em praticamente qualquer lugar que não bata sol, ela se dá muito bem em locais pouco ventilados e com baixa iluminação, locais com ar condicionado também. É uma planta tropical e não gosta de frio, então a proteja quando a temperatura cair. Essa planta requer poucos cuidados, consegue resistir muito tempo sem água, pois seu rizoma — um caule subterrâneo — consegue armazenar água. Regar 1 vez por semana é suficiente; mantenha o solo sempre úmido, mas nunca encharque — as folhas começam a ficar amarelas e a raíz morre. Adube o solo com matéria orgânica para mentê-la sempre saudável. A Zamioculca floresce no verão ou no inicio do outono, porém é muito raro ver as flores já que ficam escondidas. Mantenha a planta longe de animais ou crianças, pois é venenosa quando ingerida e a seiva pode causar irritação na pele.",
+            "dicas": ["Adube o solo a cada 3 ou 4 meses",
+                "Remova as folhas amareladas ou mortas para não afetar a saúde da planta.",
+                "Gire o vaso de vez em quando para que toda a planta receba sol.",
+                "Não limpe a planta com produtos especiais para isso, ela não se adapta. Passe café puro com um pano macio, vai deixá-las brilhantes.",
+                "Coloque um pouco de areia no fundo para garantir a drenagem.",
+                "Regue 1 a 2 vezes por semana quando estiver muito quente"
             ]
         }
     ]
