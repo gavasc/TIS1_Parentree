543b049a38097508cdca3b87a3d726dbf953b95a
Gabriel Vasconcelos <Gabriel Vasconcelos>
2019-10-23 22:07:29 -0300

    Alteração no codigo javascript de acordo com as mudanças do html e css

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 /*
     Modelo do card das plantas
-    
-    <li class="itemPlanta"> <!-- Em cada "li" terá um div contendo as infos resumidas da planta -->
-        <div class="plantaSubInfo"> <!--Modelo dos cards que serão preenchidos pelo JavaScript-->
-            <img class="fotoPlanta" src="" alt="Foto da planta">
-            <ul id="listaIconesPlanta">
-                <li id="iconeSol"><img src="" alt=""></li>
-                <li id="iconeAgua"><img src="" alt=""></li>
-                <li id="iconeDificuldade"><img src="" alt=""></li>
-            </ul>
-            <h3 class="nomePlanta"> </h3>
-            <p class="resumoDescricao"> </p>
-        </div>
-    </li>
+        
+    <div class="plantaSubInfo"> <!--Modelo dos cards que serão preenchidos pelo JavaScript-->
+        <img class="fotoPlanta" src="" alt="Foto da planta">
+        <ul id="listaIconesPlanta">
+            <li id="iconeSol"><img src="" alt=""></li>
+            <li id="iconeAgua"><img src="" alt=""></li>
+            <li id="iconeDificuldade"><img src="" alt=""></li>
+        </ul>
+        <h3 class="nomePlanta"> </h3>
+        <p class="resumoDescricao"> </p>
+    </div>
 */
 
 
 for(var i = 0; i < db.plantas.length; i++){
-    document.getElementById('listaPlantas').innerHTML += 
-    '<li class="itemPlanta">'+
-        '<div class="plantaSubInfo">'+
-            '<img class="fotoPlanta" src="" alt="Foto da planta">'+
-            '<ul id="listaIconesPlanta">'+
-                '<li id="iconeSol"><img src="" alt=""></li>'+
-                '<li id="iconeAgua"><img src="" alt=""></li>'+
-                '<li id="iconeDificuldade"><img src="" alt=""></li>'+
-            '</ul>'+
-            `<h3 class="nomePlanta"> ${db.plantas[i].nomePlanta} </h3>`+
-            `<p class="resumoDescricao"> ${db.plantas[i].resumo} </p>`+
-        '</div>'+
-    '</li>';    
+    document.getElementById('catalogoPlantas').innerHTML += 
+    
+    '<div class="plantaSubInfo">'+
+        `<img class="fotoPlanta" src="${db.plantas[i].linkImagem}" alt="Foto da planta">`+
+        '<ul id="listaIconesPlanta">'+
+            '<li id="iconeSol"><img src="" alt=""></li>'+
+            '<li id="iconeAgua"><img src="" alt=""></li>'+
+            '<li id="iconeDificuldade"><img src="" alt=""></li>'+
+        '</ul>'+
+        `<h3 class="nomePlanta"> ${db.plantas[i].nomePlanta} </h3>`+
+        `<p class="resumoDescricao"> ${db.plantas[i].resumo} </p>`+
+    '</div>';
 }
 
