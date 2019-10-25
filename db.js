20df90e2dba58b9525f0973ad3bf486a393cd08a
Gabriel Vasconcelos <Gabriel Vasconcelos>
2019-10-25 19:55:00 -0300

    Alterações e código do filtro funcionando parcialmente

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 var db = {
-    "plantas": 
+    "plantas":
     [
         {
             "nomePlanta": "Jiboia",
             "linkImagem": "",
-            "luz": 2,
-            "agua": true,
-            "nivelDificuldade": 1,
+            "categoria": "Ornamentais",
+            "luz": "Meia Luz",
+            "agua": "Pouca Agua",
+            "nivelDificuldade": "Muito Facil",
             "flores": 3,
-            "tags": ["Meia luz", "Pouca agua", "Muito Facil"],
             "resumo": "É uma das plantas mais fáceis de cuidar, suas folhas com detalhes amarelos deixam qualquer ambiente bonito.",
             "descricao": "A Jiboia é uma das melhores plantas para ter em casa, é extremamente fácil de cuidar e se desenvolve super rápido. Precisa de um solo rico em matéria orgânica. Ela é uma trepadeira, portanto, seu caule crescerá pendurado para baixo, como uma samambaia; também é uma planta resistente, consegue viver dentro ou fora de casa, até mesmo no ar condicionado, porém o mais indicado é mantê-la a meia luz, ou seja, ficar em um lugar iluminado mas sem esposição direta ao sol. É necessário regar a planta 2 a 3 vezes por semana no verão e 1 a 2 vezes no inverno. É muito fácil obter mudinhas dessa planta, corte um galho e o finque na terra ou em um pote com água e logo criará raízes. Não se esqueça que a Jiboia é uma planta tóxica, então a mantenha longe de crianças e animais.",
             "dicas": ["Manter a planta totalmente na sombra deixará suas folhas mais escuras. Não se preocupe, é normal!",
@@ -21,11 +21,11 @@ var db = {
         {
             "nomePlanta": "Peperômia melancia",
             "linkImagem": "",
-            "luz": 2,
-            "agua": true,
+            "categoria": "Ornamentais",
+            "luz": "Meia Luz",
+            "agua": "Pouca Agua",
             "nivelDificuldade": 1,
             "flores": 2,
-            "tags": ["Meia luz", "Pouca agua", "Muito Facil"],
             "resumo": "Possui uma folhagem muito linda, é pequena apesar de ser uma trepadeira.",
             "descricao": "A Peperômia melancia também é conhecida como peperômia-zebra. Suas lindas folhas em formato de coração tem listras, exatemente como uma melancia, sua folhagem é densa e seu porte é pequeno; cresce até 25 cm de altura. É ótima para decorar mesinhas de centro ou a escrivaninha do seu quarto. Nunca a deixe diretamente no sol, suas folhas queimarão. Se mostra uma grande amiga para os iniciantes pois não precisa de muitos cuidados, somente um solo com adubo orgânico, que deve ser mantido úmido, e água 1 vez por semana. A peperômia pode ser plantada o ano inteiro, porém só floresce no verão, embora as flores não sejam muito bonitas. Além de trepadeira, ela é uma planta suculenta, então suas folhas reservam um pouco de água.",
             "dicas": ["Por ser uma planta nativa e tropical, preste mais atenção nela durante o inverno.",
@@ -39,11 +39,11 @@ var db = {
         {
             "nomePlanta": "Zamioculca",
             "linkImagem": "",
-            "luz": 3,
-            "agua": true,
+            "categoria": "Ornamentais",
+            "luz": "Sombra",
+            "agua": "Pouca Agua",
             "nivelDificuldade": 1,
             "flores": 3,
-            "tags": ["Sombra", "Pouca agua", "Muito Facil"],
             "resumo": "Também é conhecida como árvore de dinheiro pelo tom verde das folhas, pode sobreviver muito tempo sem água",
             "descricao": "A Zamioculca se adapta em praticamente qualquer lugar que não bata sol, ela se dá muito bem em locais pouco ventilados e com baixa iluminação, locais com ar condicionado também. É uma planta tropical e não gosta de frio, então a proteja quando a temperatura cair. Essa planta requer poucos cuidados, consegue resistir muito tempo sem água, pois seu rizoma — um caule subterrâneo — consegue armazenar água. Regar 1 vez por semana é suficiente; mantenha o solo sempre úmido, mas nunca encharque — as folhas começam a ficar amarelas e a raíz morre. Adube o solo com matéria orgânica para mentê-la sempre saudável. A Zamioculca floresce no verão ou no inicio do outono, porém é muito raro ver as flores já que ficam escondidas. Mantenha a planta longe de animais ou crianças, pois é venenosa quando ingerida e a seiva pode causar irritação na pele.",
             "dicas": ["Adube o solo a cada 3 ou 4 meses",
--- scriptFiltros.js
+++ scriptFiltros.js
@@ -1,16 +1,100 @@
 var filtrosSelec = [];
+var filtrosCat = document.getElementsByClassName('filtroCategoria');
+var filtrosLuz = document.getElementsByClassName('filtroSol');
+var filtrosAgua = document.getElementsByClassName('filtroAgua');
 
-var filtros = document.getElementsByClassName('checkboxFiltros');
+// Varre o DB, seleciona e adiciona na página web todos as plantas que correspondem
+//aos filtros selecionados
+function comparaFiltroDB(){
+    pegaFiltros();
 
-var pegaFiltros = function(){
+    if(filtrosSelec.length != 0){
+        document.getElementById('catalogoPlantas').innerHTML = "";
+    }
+
+    db.plantas.forEach(item => {
+        
+        if(filtrosSelec.includes(item.categoria) && (filtrosSelec.includes(item.categoria)==false && nadaMarcado(filtrosCat)==false) ){
+            if(filtrosSelec.includes(item.agua) && (filtrosSelec.includes(item.categoria)==false && nadaMarcado(filtrosAgua)==false) ){
+                if(filtrosSelec.includes(item.luz) && (filtrosSelec.includes(item.categoria)==false && nadaMarcado(filtrosLuz)==false) ){
+                    console.log(item.nomePlanta);
+                    imprime(item);
+                }
+            }
+        }
+
+    });
+}
+
+// Varre o array de um tipo especifico de filtro e verifica se nenhum item do filtro
+//foi marcado
+function nadaMarcado(array){
+    for(let i = 0; i < array.length; i++){
+        if (array.checked) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Adiciona a planta passada como parametro dentro da div 'catalogoPlantas'
+//de acordo com a formatação do card de plantas
+function imprime(planta) {
+    document.getElementById('catalogoPlantas').innerHTML += 
+    
+    '<div class="plantaSubInfo">'+
+        `<img class="fotoPlanta" src="${planta.linkImagem}" alt="Foto da planta">`+
+        '<ul id="listaIconesPlanta">'+
+            '<li id="iconeSol"><img src="" alt=""></li>'+
+            '<li id="iconeAgua"><img src="" alt=""></li>'+
+            '<li id="iconeDificuldade"><img src="" alt=""></li>'+
+        '</ul>'+
+        `<h3 class="nomePlanta"> ${planta.nomePlanta} </h3>`+
+        `<p class="resumoDescricao"> ${planta.resumo} </p>`+
+    '</div>';
+}
+
+// Verifica todos os filtros e pega o nome daqueles que estão marcados e os coloca no vetor
+//de filtros selecionados
+function pegaFiltros(){
     var j = 0;
 
-    for(var i = 0; i < filtros.length; i++){
-        if(filtros[i].checked){
-            filtrosSelec[j] = filtros[i].name;
+    // filtrosCat.forEach(element => {
+    //     if (element.checked) {
+    //         filtrosSelec += element.name;
+    //     }
+    // });
+    
+    // filtrosLuz.forEach(element => {
+    //     if (element.checked) {
+    //         filtrosSelec += element.name;
+    //     }
+    // });
+
+    // filtrosAgua.forEach(element => {
+    //     if (element.checked) {
+    //         filtrosSelec += element.name;
+    //     }
+    // });
+
+    for(var i = 0; i < filtrosCat.length; i++){
+        if(filtrosCat[i].checked){
+            filtrosSelec[j] = filtrosCat[i].name;
             j++;
         }
     }
 
-    console.log(filtrosSelec);
+   for(var i = 0; i < filtrosLuz.length; i++){
+        if(filtrosLuz[i].checked){
+            filtrosSelec[j] = filtrosLuz[i].name;
+            j++;
+        }
+    }
+
+    for(var i = 0; i < filtrosAgua.length; i++){
+        if(filtrosAgua[i].checked){
+            filtrosSelec[j] = filtrosAgua[i].name;
+            j++;
+        }
+    }
 }
