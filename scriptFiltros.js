7200eb712ea1099d5b67b39a12f03ba925d3c4ba
Gabriel Vasconcelos <Gabriel Vasconcelos>
2019-10-28 21:39:56 -0300

    Mudança no script de filtros (funcionano um pouco melhor)

--- scriptFiltros.js
+++ scriptFiltros.js
@@ -1,4 +1,4 @@
-var filtrosSelec = [];
+// var filtros = document.getElementsByClassName('checkboxFiltros');
 var filtrosCat = document.getElementsByClassName('filtroCategoria');
 var filtrosLuz = document.getElementsByClassName('filtroSol');
 var filtrosAgua = document.getElementsByClassName('filtroAgua');
@@ -6,35 +6,64 @@ var filtrosAgua = document.getElementsByClassName('filtroAgua');
 // Varre o DB, seleciona e adiciona na página web todos as plantas que correspondem
 //aos filtros selecionados
 function comparaFiltroDB(){
-    pegaFiltros();
+    var filtrosSelec = pegaFiltros();
 
     if(filtrosSelec.length != 0){
         document.getElementById('catalogoPlantas').innerHTML = "";
     }
 
     db.plantas.forEach(item => {
-        
-        if(filtrosSelec.includes(item.categoria)){
-            if(filtrosSelec.includes(item.agua)) {
-                if(filtrosSelec.includes(item.luz) ){
-                    console.log(item.nomePlanta);
-                    imprime(item);
-                }
-            }
+
+        if(contemTodos(item.tags, filtrosSelec)) {
+            imprime(item);
         }
+        // if(filtrosSelec.includes(item.categoria)){
+        //     if(filtrosSelec.includes(item.agua)) {
+        //         if(filtrosSelec.includes(item.luz) ){
+        //             console.log(item.nomePlanta);
+        //             imprime(item);
+        //         }
+        //     }
+        // }
 
     });
+
+    console.log(filtrosSelec);
 }
 
-// Varre o array de um tipo especifico de filtro e verifica se nenhum item do filtro
-//foi marcado
-function nadaMarcado(array){
-    for(let i = 0; i < array.length; i++){
-        if (array.checked) {
-            return false;
-        }
+function contemTodos(tags, filtrosSelec){
+    var result = false;
+
+    if( filtrosSelec.cat.length > 0 ){
+        filtrosSelec.cat.forEach(item => {
+            if( tags.includes(item) ){
+                result = true;
+            }
+        });
     }
-    return true;
+
+    if( filtrosSelec.luz.length > 0 ){
+        filtrosSelec.luz.forEach(item => {
+            if( tags.includes(item) ){
+                result = true;
+            }
+        });
+    }
+
+    if( filtrosSelec.agua.length > 0 ){
+        filtrosSelec.agua.forEach(item => {
+            if( tags.includes(item) ){
+                result = true;
+            }
+        });
+    }
+    // filtrosSelec.forEach(item => {
+    //     if( !tags.includes(item) ) {
+    //         result = false;
+    //     }
+    // });
+
+    return result;
 }
 
 // Adiciona a planta passada como parametro dentro da div 'catalogoPlantas'
@@ -42,59 +71,56 @@ function nadaMarcado(array){
 function imprime(planta) {
     document.getElementById('catalogoPlantas').innerHTML += 
     
-    '<div class="plantaSubInfo">'+
-        `<img class="fotoPlanta" src="${planta.linkImagem}" alt="Foto da planta">`+
-        '<ul id="listaIconesPlanta">'+
-            '<li id="iconeSol"><img src="" alt=""></li>'+
-            '<li id="iconeAgua"><img src="" alt=""></li>'+
-            '<li id="iconeDificuldade"><img src="" alt=""></li>'+
-        '</ul>'+
-        `<h3 class="nomePlanta"> ${planta.nomePlanta} </h3>`+
-        `<p class="resumoDescricao"> ${planta.resumo} </p>`+
-    '</div>';
+    `<div class="plantaSubInfo">
+        <img class="fotoPlanta" src="${planta.linkImagem}" alt="Foto da planta">
+        <ul id="listaIconesPlanta">
+            <li id="iconeSol"><img src="" alt=""></li>
+            <li id="iconeAgua"><img src="" alt=""></li>
+            <li id="iconeDificuldade"><img src="" alt=""></li>
+        </ul>
+        <h3 class="nomePlanta"> ${planta.nomePlanta} </h3>
+        <p class="resumoDescricao"> ${planta.resumo} </p>
+    </div>`;
 }
 
 // Verifica todos os filtros e pega o nome daqueles que estão marcados e os coloca no vetor
 //de filtros selecionados
 function pegaFiltros(){
+
+    var filtrosSelec = {
+        'cat': [],
+        'luz': [],
+        'agua': []
+    };
+
     var j = 0;
-
-    // filtrosCat.forEach(element => {
-    //     if (element.checked) {
-    //         filtrosSelec += element.name;
+    // for(var i = 0; i < filtros.length; i++){
+    //     if(filtros[i].checked){
+    //         filtrosSelec[j] = filtros[i].name;
+    //         j++;
     //     }
-    // });
-    
-    // filtrosLuz.forEach(element => {
-    //     if (element.checked) {
-    //         filtrosSelec += element.name;
-    //     }
-    // });
-
-    // filtrosAgua.forEach(element => {
-    //     if (element.checked) {
-    //         filtrosSelec += element.name;
-    //     }
-    // });
+    // }
 
     for(var i = 0; i < filtrosCat.length; i++){
         if(filtrosCat[i].checked){
-            filtrosSelec[j] = filtrosCat[i].name;
+            filtrosSelec.cat[j] = filtrosCat[i].name;
             j++;
         }
     }
-
+    j=0;
    for(var i = 0; i < filtrosLuz.length; i++){
         if(filtrosLuz[i].checked){
-            filtrosSelec[j] = filtrosLuz[i].name;
+            filtrosSelec.luz[j] = filtrosLuz[i].name;
+            j++;
+        }
+    }
+    j=0;
+    for(var i = 0; i < filtrosAgua.length; i++){
+        if(filtrosAgua[i].checked){
+            filtrosSelec.agua[j] = filtrosAgua[i].name;
             j++;
         }
     }
 
-    for(var i = 0; i < filtrosAgua.length; i++){
-        if(filtrosAgua[i].checked){
-            filtrosSelec[j] = filtrosAgua[i].name;
-            j++;
-        }
-    }
+    return filtrosSelec;
 }
