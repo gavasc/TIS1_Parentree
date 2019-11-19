3de375af8a14cdf336fbdb821a55fb9a11daa996
João Silveira <jotabtw@gmail.com>
2019-11-19 14:57:28 -0300

    Melhoras no CSS do INFOPLANTA

--- infoPlanta/script.js
+++ infoPlanta/script.js
@@ -11,7 +11,7 @@ db.plantas.forEach(item => {
 			<a id="nome_cientifico"> "${item.nomeCientifico}"</a>
 			<img id="imgPrincipalPlanta" src="${item.linkImagem}">
 			<div id="descricao"><a>"${item.descricao}"</a></div>
-			<a id="tags">Encontrado em:<br>"${item.tags}"</a>
+			<a id="tags">Filtrado por:<p>"${item.tags}"</p></a>
         `;
     }
 });
