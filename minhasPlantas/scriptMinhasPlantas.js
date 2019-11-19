0dfba14cdd7f17a094d492b01d14dd52e911b182
Gabriel Vasconcelos <Gabriel Vasconcelos>
2019-11-19 20:01:15 -0200

    Script para mostrar as plantas salvas no localStorage na tela Minhas Plantas

---
+++ minhasPlantas/scriptMinhasPlantas.js
@@ -0,0 +1,27 @@
+var listaPlantas = [];
+
+if ( localStorage.getItem('minhasPlantas') === null ) {
+    alert('Você não possui plantas ainda :(');
+} else {
+    listaPlantas = JSON.parse(localStorage.getItem('minhasPlantas'));
+    
+    for(let i = 0; i < listaPlantas.length; i++){
+        pegaInfos(listaPlantas[i]);
+    }
+}
+
+function pegaInfos(nome){
+    for(let i = 0; i < db.plantas.length; i++){
+        if(db.plantas[i].nomePlanta == nome) mostrar(db.plantas[i]);
+    }
+}
+
+function mostrar(item){
+    document.getElementById('listaMinhasPlantas').innerHTML += `
+        <div class="minhaPlanta">
+            <h2 class="nomePlanta"> ${item.nomePlanta} </h2>
+            <img class="imagemPlanta" src="${item.linkImagem}">
+            <p class="resumoPlanta"> ${item.resumo} </p> 
+        </div>
+    `
+}
